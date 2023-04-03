64beb5b381c81d8d3ce711857146e00810907e20
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-04-03 22:37:26 +0300

    add main files for execution

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,4 +7,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """this function runs wait_random n times"""
-    return sorted(await asyncio.gather(*(wait_random(max_delay) for _ in range(n))))
+    tasks: List[any] = []
+    for i in range(n):
+        tasks.append(wait_random(max_delay))
+    return sorted(await asyncio.gather(*tasks))
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,5 +7,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """this function runs wait_random n times"""
-    tasks: List[float] = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
-    return sorted(tasks)
+    tasks: List[any] = []
+    for i in range(n):
+        tasks.append(task_wait_random(max_delay))
+    return sorted(await asyncio.gather(*tasks))
