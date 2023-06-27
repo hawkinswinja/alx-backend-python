823b30ecd0715f3e2056b05287afee5487cb4240
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-06-27 15:57:03 +0300

    update to handle key error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,27 +3,28 @@
 """
 from parameterized import parameterized
 import unittest
-from utils import access_nested_map, Mapping, Sequence
+from utils import access_nested_map
+from typing import (Sequence, Mapping, Union)
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test class"""
-    @parameterized.expand([({'a': 1}, ('a',)),
-                          ({'a': {'b': 2}}, ('a',)),
-                          ({'a': {'b': 2}}, ('a', 'b'))])
-    def test_access_nested_map(self, test: Mapping, path: Sequence) -> None:
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2),
+    ])
+    def test_access_nested_map(self, test: Mapping, path: Sequence,
+                               expected: Union[Mapping, int]) -> None:
         """Tests the map for the key in path"""
-        self.assertEqual(access_nested_map(test, path),  get_val(test, path))
+        self.assertEqual(access_nested_map(test, path),  expected)
 
-    @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
     def test_access_nested_map_exception(self, test: Mapping,
                                          path: Sequence) -> None:
         """raise exception error"""
-        self.assertRaises(KeyError, access_nested_map(test, path))
-
-
-def get_val(data: Mapping, path: Sequence) -> any:
-    """get value from data using key"""
-    for key in path:
-        data = data[key]
-    return data
+        with self.assertRaises(KeyError):
+            access_nested_map(test, path)
