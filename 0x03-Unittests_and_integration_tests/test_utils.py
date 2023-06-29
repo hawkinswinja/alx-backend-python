5420e1f3268beff84ddf7ee9c507cc05735ef769
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-06-29 15:38:38 +0300

    update to use mock for requests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """test_utils module
 """
-from parameterized import parameterized
 import unittest
-from utils import access_nested_map
+from unittest import mock
+from parameterized import parameterized
+from utils import (access_nested_map, get_json, memoize)
 from typing import (Sequence, Mapping, Union)
 
 
@@ -28,3 +29,39 @@ class TestAccessNestedMap(unittest.TestCase):
         """raise exception error"""
         with self.assertRaises(KeyError):
             access_nested_map(test, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """test module for json"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url: str, resp: Mapping) -> None:
+        """test the utils.get_json function"""
+        with mock.patch('utils.requests.get') as mock_get:
+            mock_get.return_value.json.return_value = resp
+            self.assertEqual(get_json(url), resp)
+            mock_get.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """test module for wrapper memoize"""
+    def test_memoize(self) -> None:
+        """unit test case for memoization"""
+        class TestClass:
+            def a_method(self) -> int:
+                return 42
+
+            @memoize
+            def a_property(self) -> int:
+                return self.a_method()
+
+        test = TestClass()
+        with mock.patch.object(test, 'a_method') as me:
+            me.return_value = 30
+            a = test.a_property
+            b = test.a_property
+            self.assertEqual(a, 30)
+            self.assertEqual(b, 30)
+            me.assert_called_once()
