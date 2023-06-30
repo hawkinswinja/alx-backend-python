0491588504a85706fe5072488dfb09c3ae87508d
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-06-30 12:59:35 +0300

    update unit tests for property methods

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -19,3 +19,34 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.return_value = 'Successfull ' + org
         self.assertEqual(data.org, 'Successfull ' + org)
         mock_get_json.assert_called_once_with(data.ORG_URL.format(org=org))
+
+    def test_public_repos_url(self):
+        """unit test for method _public_repos_url"""
+        with mock.patch.object(GithubOrgClient, 'org',
+                               new_callable=mock.PropertyMock) as payload:
+            payload.return_value = {'repos_url': 'test data success'}
+            a = GithubOrgClient('test')._public_repos_url
+            self.assertEqual(a, 'test data success')
+
+    @mock.patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """unit test for public repos"""
+        with mock.patch.object(GithubOrgClient, '_public_repos_url',
+                               new_callable=mock.PropertyMock) as url:
+            url.return_value = 'myrepos'
+            mock_get_json.return_value = [
+                {'name': 'test'},
+                {'name': 'test1'},
+                {'name': 'test2'}
+            ]
+            data = GithubOrgClient('test').public_repos()
+            mock_get_json.assert_called_once_with('myrepos')
+            self.assertEqual(data, ['test', 'test1', 'test2'])
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, key, expected):
+        """unit test for static has_license"""
+        self.assertEqual(GithubOrgClient.has_license(repo, key), expected)
